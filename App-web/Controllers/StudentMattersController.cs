9d1b04697f3cd77678ac541ac6310f4ea07d0bc8
ayrton cravero <ayrtoncravero26@gmail.com>
2020-11-12 12:39:19 -0300

    fixed editing of student matter

--- App-web/Controllers/StudentMattersController.cs
+++ App-web/Controllers/StudentMattersController.cs
@@ -76,21 +76,37 @@ namespace App_web.Controllers
         }
 
         // GET: StudentMatters/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var studentMatter = await _context.StudentMatters.FindAsync(id);
-            if (studentMatter == null)
+        //    var studentMatter = await _context.StudentMatters.FindAsync(id);
+        //    if (studentMatter == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewData["MatterId"] = new SelectList(_context.Matters, "Id", "Id", studentMatter.MatterId);
+        //    ViewData["StudentId"] = new SelectList(_context.Students, "Id", "Id", studentMatter.StudentId);
+        //    return View(studentMatter);
+        //}
+
+        public async Task<IActionResult> Edit(int? idStudent, int? idMatter)
+        {
+            if (idStudent == null || idMatter == null)
             {
                 return NotFound();
             }
-            ViewData["MatterId"] = new SelectList(_context.Matters, "Id", "Id", studentMatter.MatterId);
-            ViewData["StudentId"] = new SelectList(_context.Students, "Id", "Id", studentMatter.StudentId);
-            return View(studentMatter);
+            var studentmatter = await _context.StudentMatters.FindAsync(idStudent, idMatter);
+            if (studentmatter == null)
+            {
+                return NotFound();
+            }
+            ViewData["estudianteId"] = new SelectList(_context.Students, "id", "apellido", studentmatter.StudentId);
+            ViewData["materiaId"] = new SelectList(_context.Matters, "id", "nombre", studentmatter.MatterId);
+            return View(studentmatter);
         }
 
         // POST: StudentMatters/Edit/5
